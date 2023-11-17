e4c1b81c175dc41cb41b97f40383e26534edced8
owais-hacker <142396017+owais-hacker@users.noreply.github.com>
2023-11-16 21:52:19 -0800

    Add files via upload
    done

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function run(){
+    let htmlCode = document.querySelector('.html-code').value;
+    let cssCode = document.querySelector('.css-code').value;
+    let jsCode = document.querySelector('.js-code').value;
+    let output = document.querySelector('.output');
+
+
+    output.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "</style>";
+    output.contentWindow.eval(jsCode);
+}
+setInterval(() => {
+    run()
+}, 800);
